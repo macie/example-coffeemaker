f08b56549e202675680c004c183b14fd489c1e00
macie <macie@users.noreply.github.com>
2018-10-03 19:23:55 +0200

    Add support for test && commit || revert

---
+++ test-commit-revert.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# test && commit || revert
+#   Commit changes only if all tests pass. Otherwise revert code
+#   to starting point.
+#   Tool for npm and git.
+#
+# Usage:
+#   $ ./test-commit-revert.sh "Content of commit message"
+#
+# Copyright (c) 2018 Maciej Żok <maciek.zok@gmail.com>
+# MIT License (http://opensource.org/licenses/MIT)
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+has_commit_msg="${#} -gt 0"
+commit_msg=${@}
+
+if [ ! ${has_commit_msg} ]; then
+  echo "ERROR: No commit message"
+  exit 64  # command line usage error (via /usr/include/sysexits.h)
+fi
+
+npm test && git commit -am ${commit_msg} || (echo "ERROR: Tests failed -> revert all changes"; git reset --hard)
